ce42e9d225bef2bb066caf9055e1f4d55c05ac3a
Hendro Wibowo <hendrothemail@gmail.com>
2025-04-27 07:36:51 +0700

    Fix auth issue

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -2,7 +2,9 @@ from jose import jwt
 from passlib.context import CryptContext
 from datetime import datetime, timedelta, timezone
 from ..settings import settings
-
+from ..models.user import User
+from ..database import AsyncSessionLocal
+from sqlalchemy.future import select
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 
@@ -44,7 +46,7 @@ def jwt_decode(token: str):
             detail="Could not validate credentials"
         )
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
+async def get_current_user(token: str = Depends(oauth2_scheme)) -> User:
     payload = jwt_decode(token)
     username = payload.get("sub")
     if username is None:
@@ -52,4 +54,13 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid authentication credentials"
         )
-    return username
+
+    async with AsyncSessionLocal() as db:
+        result = await db.execute(select(User).where(User.username == username))
+        user = result.scalars().first()
+        if user is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="User not found"
+            )
+        return user
